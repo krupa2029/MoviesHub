33e9a30e8dda8fde296830119780949dda144403
krupa2029 <kjkotia10@gmail.com>
2021-12-09 15:43:42 +0530

    Generate Movies-List with original fetched data Passing latestmovie data to the MovieCard

--- src/components/Movies/MoviesList.js
+++ src/components/Movies/MoviesList.js
@@ -1,62 +1,21 @@
-import movieimg from "../img/img_m.jpg";
 import MoviesCard from "./MovieCard";
-import classes from './MoviesList.module.css';
+import classes from "./MoviesList.module.css";
 
 const MoviesList = (props) => {
-  const DUMMY_DATA = [
-    {
-      id: "1",
-      title: "Resident Evil",
-      subtitle: "Vendetta",
-      imdb_rating: "8.1",
-      genre: "Sci-fi",
-      imgsrc:  movieimg ,
-      release_year: '2019',
-    },
-    {
-        id: "2",
-        title: "Resident Evil",
-        subtitle: "Vendetta",
-        imdb_rating: "8.1",
-        genre: "Sci-fi",
-        imgsrc:  movieimg ,
-        release_year: '2019',
-      },
-      {
-        id: "3",
-        title: "Resident Evil",
-        subtitle: "Vendetta",
-        imdb_rating: "8.1",
-        genre: "Sci-fi",
-        imgsrc:  movieimg ,
-        release_year: '2019',
-      },
-      {
-        id: "4",
-        title: "Resident Evil",
-        subtitle: "Vendetta",
-        imdb_rating: "8.1",
-        genre: "Sci-fi",
-        imgsrc:  movieimg ,
-        release_year: '2019',
-      },
-  ];
-
   return (
-    <section className={classes['movies-list']}>
-      {DUMMY_DATA.map((moviedata) => (
+    <ul className={classes["movies-list"]}>
+      {props.MoviesData.results.map((moviedata) => (
         <MoviesCard
           key={moviedata.id}
           id={moviedata.id}
           title={moviedata.title}
-          subtitle={moviedata.subtitle}
-          imdb_rating={moviedata.imdb_rating}
-          genre={moviedata.genre}
-          imgsrc={moviedata.imgsrc}
-          release_year={moviedata.release_year}
+          imdb_rating={moviedata.vote_average}
+          // genre={moviedata.genres}
+          imgsrc={moviedata.poster_path}
+          release_year={moviedata.release_date}
         />
       ))}
-    </section>
+    </ul>
   );
 };
 export default MoviesList;
