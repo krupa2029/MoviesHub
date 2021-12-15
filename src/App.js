a2e2cb515f5aaa9a51b20dd50406f3b4c6e3994e
krupa2029 <kjkotia10@gmail.com>
2021-12-15 18:49:32 +0530

    Added Lazy loading

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router";
 import Layout from "./components/Layout/Layout";
 import LoadingSpinner from "./components/UI/LoadingSpinner/LoadingSpinner";
-import HomePage from "./pages/HomePage";
-import MovieDetailPage from "./pages/MovieDetailPage";
-import NotFound from "./pages/NotFound";
-import SearchedMovie from "./pages/SearchedMovie";
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const MovieDetailPage = React.lazy(() => import("./pages/MovieDetailPage"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const SearchedMovie = React.lazy(() => import("./pages/SearchedMovie"));
 
 function App() {
   return (
-    
     <Layout>
-    <Suspense
-      fallback={
-        <div className="centered">
-          <LoadingSpinner />
-        </div>
-      }
-    >
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/movies" />
-        </Route>
-        <Route path="/movies" exact>
-          <HomePage />
-        </Route>
-        <Route path="/movies/:movieId">
-          <MovieDetailPage />
-        </Route>
-        <Route path="/search/:search_term">
-          <SearchedMovie/>
-        </Route>
-       
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Suspense>
-  </Layout>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/movies" />
+          </Route>
+          <Route path="/movies" exact>
+            <HomePage />
+          </Route>
+          <Route path="/movies/:movieId">
+            <MovieDetailPage />
+          </Route>
+          <Route path="/search/:search_term">
+            <SearchedMovie />
+          </Route>
+
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Layout>
   );
 }
 
