f9132da96a9f1c544a4b9181efef1e1185c75e2d
krupa2029 <kjkotia10@gmail.com>
2021-12-15 15:09:13 +0530

    Handling Loading and error status

--- src/components/Movies/LatestMovies.js
+++ src/components/Movies/LatestMovies.js
@@ -3,33 +3,44 @@ import classes from "./Movies.module.css";
 import useHttp from "../../hooks/use-http";
 import { getLatestMovies } from "../../lib/api";
 import { useEffect } from "react";
+import LoadingSpinner from "../UI/LoadingSpinner/LoadingSpinner";
 
 const LatestMovies = () => {
+  // Passing getLatestMovies Function in useHttp hook to send Http request for it...
+  const {
+    sendRequest,
+    status,
+    data: loadedLatestMovies,
+    error,
+  } = useHttp(getLatestMovies, true);
 
-   // Passing getLatestMovies Function in useHttp hook to send Http request for it...
-    const {
-      sendRequest,
-      status,
-      data: loadedLatestMovies,
-      error,
-    } = useHttp(getLatestMovies, true);
-  
-    useEffect(() => {
-      sendRequest();
-    }, [sendRequest]);
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
 
-    if (status === "completed" && (loadedLatestMovies  && loadedLatestMovies !== null)) {
-      console.log(loadedLatestMovies);
-      return (
-        <div className={classes["movies-container"]}>
-          <h2>Latest Movies</h2>
-          <MoviesList MoviesData={loadedLatestMovies}/>
-        </div>
-      );
-    }
-    else{
-      return<p>{error}</p>
-    }
-  
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (
+    status === "completed" &&
+    loadedLatestMovies &&
+    loadedLatestMovies !== null
+  ) {
+    console.log(loadedLatestMovies);
+    return (
+      <div className={classes["movies-container"]}>
+        <h2>Latest Movies</h2>
+        <MoviesList MoviesData={loadedLatestMovies} />
+      </div>
+    );
+  }
 };
 export default LatestMovies;
